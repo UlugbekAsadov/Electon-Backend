4e52d67e01d88936027c33f8a594be5914b57a7e
Ulugbek laptop <ulugb3k270@gmail.com>
2023-06-10 15:14:59 +0500

    feat:added test router

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-
 const app = express();
-
 dotenv.config();
+
 const PORT = process.env.PORT || 9000;
+
+app.get("/", (req, res) => {
+  res.send("GET request to the homepage");
+});
 
 app.listen(() => {
   mongoose
